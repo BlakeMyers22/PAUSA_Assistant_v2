ddf6d26a777366304fd6207a33c02eccda07f3ce
BlakeMyers22 <167836117+BlakeMyers22@users.noreply.github.com>
2025-01-04 20:17:12 -0600

    Delete scripts directory

--- scripts/config.js
+++
@@ -1,7 +0,0 @@
-// scripts/config.js
-
-// WARNING: Any keys you place here WILL be visible in your published GitHub Pages site.
-// This is not recommended for production usage.
-const OPENAI_API_KEY = "YOUR_OPENAI_API_KEY_HERE";
-const WEATHER_API_KEY = "YOUR_WEATHER_API_KEY_HERE";
-
